cc1bcc714e9a662a767cc7fcd2e814e498aac11f
AL QUINIÑEZA <alquinineza14@gmail.com>
2026-06-04 20:15:10 +0800

    Add simple login system using trigraphs
    This code implements a simple login system using trigraphs, which is not recommended. It prompts the user for a username and password, and checks them against predefined values.

---
+++ login.c
@@ -0,0 +1,40 @@
+??=include <stdio.h>
+??=include <string.h>
+??=include <stdbool.h>
+
+// Disclaimer this is not good example. Using Trigraphs is not recommend. I just use it for extra typing in the keyboard.
+// It may contain typo err
+// I'm using Android browser to write this in Github.
+
+//.When compiling don't forget to add "-trigraphs" since I use trigraphs for this
+
+int main()??<
+
+  CONST char USER??(20??) = "admin";
+  CONST char PASSWORD??(20??) = "pass123";
+
+  char userName??(20??) = "";
+  char userPassword??(20??) = "";
+
+  bool isLogIn = true;
+
+  while(isLogIn)??<
+    printf("Enter your Username : ");
+    scanf("%s", userName);
+
+    printf("Enter your Password : ");
+    scanf("%s", userPassword);
+
+    if(strcmp(USER, userName) == 0 && strcmp(PASSWORD, userPassword) == 0)??<
+      printf("Login Successfully!??/n");
+      isLogIn = false;
+    ??>
+    else??<
+      printf("Wrong credentials! please try again.??/n");
+    ??>
+  ??>
+
+  printf("***Welcome back*"");
+
+  return 0;
+??>
