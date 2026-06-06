1696bc613f3d7ebe261b72ed11667faba17173d2
AL QUINIÑEZA <alquinineza14@gmail.com>
2026-06-06 23:24:19 +0800

    Implement rectangle drawing with user input
    Fix input handling and loop logic for rectangle printing.

---
+++ 2-dimensional-rectangles.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+// I use trigraphs in this
+// add -trigraphs when compiling 
+int main()??<
+
+   int rows = 0;
+   int columns = 0;
+   int symbol = 0;
+    
+   printf("Enter the # of rows: "); 
+   scanf("%d", &rows);
+
+   printf("Enter the # of columns: ");   
+   scanf("%d", columns);   
+
+   printf("Enter a symbol: ");   
+   // You need to clear the input buffer when inputing character just pust space " %c"
+   scanf(" %c", symbol);
+    
+   for(int i = 0; i < rows; i++)??<  
+      for(int j = 0; j < columns; i++)??<
+         printf("%c", symbol);
+      ??>
+      printf("??/n");
+   ??>  
+  
+
+??>
