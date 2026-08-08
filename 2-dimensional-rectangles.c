f258ffb00237772055a4b859d435325a9d8c6d95
AL QUINIÑEZA <alquinineza14@gmail.com>
2026-08-08 23:22:08 +0800

    Fix typo in variable initialization comment

--- 2-dimensional-rectangles.c
+++ 2-dimensional-rectangles.c
@@ -2,7 +2,7 @@
 // I use trigraphs in this mini project 
 // add -trigraphs flag when compiling ex. gcc filename.c -o filename - trigraphs
 int main()??<
-   //Initialize variables
+   //Initialize variablesssssss
    int rows = 0;
    int columns = 0;
    int symbol = 0;
